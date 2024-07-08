70ce830059a6ee232501432b55c2ef08d2037028
EldHasp <eldhasp@gmail.com>
2024-07-08 19:34:43 +0300

    Repeating the example from the question.

---
+++ GridLengthUserControl/GridLengthUserControl/MyUserControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GridLengthUserControl
+{
+    public partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public GridLength IconColumnWidth
+        {
+            get { return (GridLength)GetValue(IconColumnWidthProperty); }
+            set { SetValue(IconColumnWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconColumnWidthProperty =
+            DependencyProperty.Register(nameof(IconColumnWidth),
+                                        typeof(GridLength),
+                                        typeof(MyUserControl),
+                                        new PropertyMetadata(new GridLength(0),
+                                                             new PropertyChangedCallback(SetIconColumnWidth)));
+
+        public static void SetIconColumnWidth(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            MyUserControl target = (MyUserControl)d;
+            target.MainGrid.ColumnDefinitions[0].Width = ((GridLength)e.NewValue);
+        }
+    }
+}
